34a9701798a13e9bce8036b210f62a92aa6e37c3
Taryn Li <tarynli@hotmail.com>
2018-12-18 15:48:11 -0500

    first commit

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require('express'),
+app = express(),
+cors = require('cors');
+
+dotenv.config(); 
+
+app.use(cors());
+
+const PORT = process.env.PORT;
+
+
+
+
+
+
+app.listen(PORT, (err) => {
+    if (err) {
+        return console.log(err);
+    }
+    console.log(`server is listening on ${PORT}...hello!`)
+})
