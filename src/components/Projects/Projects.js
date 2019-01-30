357c9a27163572b6494429ff5524bbd5180c551e
Taryn Li <tarynli@hotmail.com>
2019-01-30 14:11:10 -0500

    page styling

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,19 +1,34 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
-import './projects.scss'
+import farmr from '../../assets/Farmr-macbook.png';
 import TopNav from '../Nav/TopNav';
+import './projects.scss'
 
 
 export default class Projects extends Component {
-  render() {
-    return (
-      <section>
-          <TopNav />
-        <div className='projects'>
-        <h1>Below are some projects I've worked on</h1>
-        </div>
-      </section>
-    )
-  }
+    render() {
+        return (
+            <section>
+                <TopNav />
+                <div className='projects'>
+                    <h1>Below are some projects I've worked on</h1>
+                    <div className='table'>
+                    <ul>
+                        <li>
+                            <Link to={'/farmr'}>
+                                <img src={farmr} alt='farmr screenshot' />
+                            </Link>
+                        </li>
+                        <li>
+                            <p>Coming soon</p>
+                        </li>
+                        <li>
+                            <p>Coming soon</p>
+                        </li>
+                    </ul>
+                    </div>
+                </div>
+            </section>
+        )
+    }
 }
-
